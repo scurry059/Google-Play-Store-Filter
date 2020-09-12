5ff77827249a2c74cee68cd86f7b241979576d45
Matthew Steuerer <matthew.steuerer@gmail.com>
2020-09-11 22:44:11 -0400

    Initial Commit
    Uploading Python File for project and csv files that serve as data source for filter

---
+++ Google Play Store Filter.py
@@ -0,0 +1,5 @@
+# Google Play Store Data Filter
+# Authored by Matthew Steuerer, Nikhil Sankepalli, and Seth Curry
+# October 7th, 2020
+# Demonstrates the use of Lists is Python with 2018 Google Play Store Data
+# Data Source URL: https://www.kaggle.com/lava18/google-play-store-apps
