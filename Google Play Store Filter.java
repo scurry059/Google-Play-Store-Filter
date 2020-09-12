509b2b1844e39ec5c124b5f4aa63eb00ae1a6fd5
Matthew Steuerer <matthew.steuerer@gmail.com>
2020-09-11 23:21:23 -0400

    Uploading Java File for Java Version

---
+++ Google Play Store Filter.java
@@ -0,0 +1,18 @@
+/*
+
+Google Play Store Data Filter
+Authored by Matthew Steuerer, Nikhil Sankepalli, and Seth Curry
+October 7th, 2020
+Demonstrates the use of Lists is Python with 2018 Google Play Store Data
+Data Source URL: https://www.kaggle.com/lava18/google-play-store-apps
+
+Initial Ideas: create a list of objects where each entry in the list is an app object
+Each app will have attributes that correspond to column titles
+The user needs to be able to filter on up to three of these attributes
+Need to confirm if user should be able to apply multiple filters (ex: category type and content rating)
+User inputs their criteria and the program needs to find all records that match
+Minimum Requirements: Be able to run the program once and return result, user may have to rerun for additional queries
+Ideal Requirements: Be able to have program run in a loop and filter on multiple criteria
+Shoot for the Stars Requirements: Python GUI with program logic
+
+ */
