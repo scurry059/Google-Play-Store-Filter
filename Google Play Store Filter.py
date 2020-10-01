29ae280639a63b64ae90daf7ccd19747c2309c40
Matthew Steuerer <matthew.steuerer@gmail.com>
2020-09-30 22:19:30 -0400

    Revert to last known working commit
    trying to revert to a working copy

--- Google Play Store Filter.py
+++ Google Play Store Filter.py
@@ -8,6 +8,38 @@
 
 import csv
 
+<<<<<<< HEAD
+=======
+
+# Class to represent an app object and all its properties (columns) from the data source
+
+
+class GoogleApp:
+    def __init__(self, app, category, rating, reviews, size, installs, type, price, contentRating, genres, lastUpdated,
+                 currentVer, androidVer):
+        self.app = app
+        self.category = category
+        self.rating = rating
+        self.reviews = reviews
+        self.size = size
+        self.installs = installs
+        self.type = type
+        self.price = price
+        self.contentRating = contentRating
+        self.genres = genres
+        self.lastUpdated = lastUpdated
+        self.currentVer = currentVer
+        self.androidVer = androidVer
+
+    # String Representation of Google App Object (How its displayed once its printed)
+    def __str__(self):
+        appString = self.app + " " + self.category + " " + self.rating + " " + self.reviews + " "
+        appString = appString + " " + self.size + " " + self.installs + " " + self.type + " "
+        appString = appString + " " + self.price + " " + self.contentRating + " " + self.genres + " "
+        appString = appString + " " + self.lastUpdated + " " + self.currentVer + " " + self.androidVer
+        return appString
+
+>>>>>>> parent of 8a95aa2... Scrap Object Approach
 # Our list object which will hold the entire data source for processing
 appList = []
 
@@ -18,6 +50,7 @@ with open('googleplaystore.csv') as csvFile:
     for row in readCSV:
         # Note: needed to edit data source as one row had two columns that were null and reader object threw errors
         # For each row in the file, make an object and append it to the list of items
+<<<<<<< HEAD
         listFormOfApp = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10],
                          row[11], row[12]]
         print(listFormOfApp)
@@ -31,8 +64,17 @@ def displayColumns():
           "Updated Date \n")
     print("12. Current Version (Only checks major versions, not minor) \t 13. Android Version (minimum Android OS "
           "required, also only checks major version \n")
+=======
+        appList.append(
+            GoogleApp(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11],
+                      row[12]))
+        # The first object in the list is the column headings, and could be ignored
+        # just used for debugging print(appList[0])
+>>>>>>> parent of 8a95aa2... Scrap Object Approach
 
+        # just used for debugging i = i + 1
 
+<<<<<<< HEAD
 def searchOneTerm(columnInput1, searchList, searchedTerm1):  # the input1 here is the column to search
     length = len(searchList)
     index1 = 0
@@ -87,8 +129,15 @@ def searchThreeTerms(columnInput1, columnInput2, columnInput3, searchList, searc
     print("Total apps matching your search: " + str(totalMatchingRows))
 
 
+=======
+# No more needs to be done to load the source
+# Now just need to offer a menu with search options for user;
+# Different options depending on which column they want to search on
+>>>>>>> parent of 8a95aa2... Scrap Object Approach
 # Requirements state user must be able to search by up to three values (Need to check up to 3 columns)
 # Ask user how many search values they will enter (up to three allowed)
+# Have a switch statement depending on which columns user would like to search by
+# Have the switch run in a loop with an exit command
 
 # First, prompt and ask user how many key values they will search for
 
@@ -101,10 +150,11 @@ print("Then, you will be prompted for your search term. Enter the term, followed
 exitCondition = False  # exit condition to terminate menu loop
 while not exitCondition:
     numberOfSearchTerms = int(input("Would you like to use 1, 2, or 3 search terms? Please type the number and press "
-                                    "enter. Enter 4 to exit the program. \n"))
+                                    "enter. Enter 4 to exit the program."))
     if numberOfSearchTerms == 1:
         # offer all columns and let user pick one to search on
         # then search the entire list at that columns index and return all that match
+<<<<<<< HEAD
         displayColumns()
         input1 = int(input("Which column above would you like to search on? \n"))
         searchTerm1 = input("What term do you wish to search for? \n")
@@ -128,5 +178,23 @@ while not exitCondition:
         searchTerm2 = input("What is the second term you wish to search for? \n")
         searchTerm3 = input("What is the third term you wish to search for? \n")
         searchThreeTerms(input1, input2, input3, appList, searchTerm1, searchTerm2, searchTerm3)
+=======
+        # display columns function
+        input1 = input("Which column above would you like to search on?")
+        # searchOneTerm function
+    elif numberOfSearchTerms == 2:
+        # display columns function
+        input1 = input("Select the first column you would like to search on: ")
+        input2 = input("Select the second column you would like to search on: ")
+        # searchTwoTerms(input1, input2)function
+    elif numberOfSearchTerms == 3:
+        # display columns function
+        input1 = input("Select the first column you would like to search on: ")
+        input2 = input("Select the second column you would like to search on: ")
+        input3 = input("Select the third column you would like to search on: ")
+        # searchThreeTerms(input1, input2, input3) function
+>>>>>>> parent of 8a95aa2... Scrap Object Approach
     elif numberOfSearchTerms == 4:
         exitCondition = True
+
+
